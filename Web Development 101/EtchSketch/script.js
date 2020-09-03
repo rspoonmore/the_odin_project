00d2c4c8e5de2c418bfe8aa954f8242937a264c1
rspoonmore <rspoonmore@gmail.com>
2020-09-03 13:26:41 -0500

    Basic board and functionality added in

--- Web Development 101/EtchSketch/script.js
+++ Web Development 101/EtchSketch/script.js
@@ -1,22 +1,48 @@
-let numCells = 3;
-let cellSize = 50;
-let columnWidths = '';
-let rowHeights = '';
-for(i=0; i < numCells; i++){
-    columnWidths += (cellSize + 'px ')
-    rowHeights += (numCells * cellSize + 'px ')
+let numCells = 100;
+boardSize = 1000;
+
+function addCellToBoard(rowDiv) {
+    let newCell = document.createElement('div');
+            newCell.classList.add('cell');
+            newCell.id = `${i}-${j}`;
+            newCell.addEventListener('mouseenter', () => {newCell.classList.add('hovered')});
+            rowDiv.appendChild(newCell);
 }
-let cellContainer = document.querySelector('.cellContainer')
-cellContainer.style.cssText = 'grid-template-columns: ' + columnWidths + '; grid-template-rows: ' + rowHeights + ';';
-console.log(rowHeights);
-for(i = 0; i < numCells; i++){
-    let rowDiv = document.createElement('div');
-    //rowDiv.style.display = 'inline';
-    for(j = 0; j < numCells; j++){
-        let newCell = document.createElement('div');
-        newCell.classList.add('cell');
-        newCell.id = `${i}-${j}`
-        rowDiv.appendChild(newCell);
+
+function buildBoard(numCells, boardSize) {
+    let cellSize = Math.floor(boardSize / numCells);
+    let columnWidths = '';
+    for(i=0; i < numCells; i++){
+        columnWidths += (cellSize + 'px ');
     }
-    cellContainer.appendChild(rowDiv);
+    let cellContainer = document.querySelector('.cellContainer')
+    cellContainer.style.cssText = 'grid-template-columns: ' + columnWidths + ';';
+    document.documentElement.style.setProperty('--cell-size', (cellSize - 2) + 'px');
+    document.documentElement.style.setProperty('--board-size', boardSize + 'px');
+
+    for(i = 0; i < numCells; i++){
+        let rowDiv = document.createElement('div');
+        for(j = 0; j < numCells; j++){
+            addCellToBoard(rowDiv);
+        }
+        cellContainer.appendChild(rowDiv);
+    }
 }
+
+
+function resetView() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.classList.remove('hovered');
+    });
+}
+
+
+function game() {
+    buildBoard(numCells, boardSize)
+    resetButton = document.getElementById('resetButton');
+    resetButton.addEventListener('click', resetView);
+}
+
+game()
+
